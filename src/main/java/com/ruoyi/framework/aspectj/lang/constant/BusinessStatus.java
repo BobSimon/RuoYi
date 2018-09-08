d3852942ce1fa82847f617ce111b09dfe65ce33f
Bob Simon <843290769@qq.com>
2018-09-08 15:06:01 +0800

    第一次远程提交代码

---
+++ src/main/java/com/ruoyi/framework/aspectj/lang/constant/BusinessStatus.java
@@ -0,0 +1,16 @@
+package com.ruoyi.framework.aspectj.lang.constant;
+
+/**
+ * @desc 业务操作状态值
+ * @author Bob Simon
+ */
+public class BusinessStatus {
+    /** 其它 */
+    public static final String OTHER = "-1";
+
+    /** 成功 */
+    public static final String SUCCESS = "0";
+
+    /** 失败 */
+    public static final String FAIL = "1";
+}
